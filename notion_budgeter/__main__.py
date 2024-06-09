b18862be39f84f405f432b8883f0da04a69d1f70
Evan J <42357644+ejach@users.noreply.github.com>
2024-06-08 22:03:29 -0400

    Fix (smh)

--- notion_budgeter/__main__.py
+++ notion_budgeter/__main__.py
@@ -4,9 +4,8 @@ from notion_budgeter.notion_budgeter.notion_budgeter import send_to_notion
 from schedule import every, run_pending
 
 if __name__ == '__main__':
-    send_to_notion()
-    # every().minute.do(send_to_notion)
-    #
-    # while True:
-    #     run_pending()
-    #     sleep(1)
+    every().minute.do(send_to_notion)
+
+    while True:
+        run_pending()
+        sleep(1)
